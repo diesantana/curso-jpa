6b20e98694167148201f360d6af5cf0ec21f0fed
Diego Santana <3c.santana@gmail.com>
2024-03-25 08:07:14 -0300

    feat: add o mapeamento JPA e removendo o Id do construtor

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,18 +1,48 @@
 package application;
 
 import domain.Person;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Person p1 = new Person(1, "Carlos Orange", "carlos@gmail.com");
-		Person p2 = new Person(2, "Bob Brown", "bob@gmail.com");
-		Person p3 = new Person(3, "Maria Green", "maria@gmail.com");
+		Person p1 = new Person("Carlos Orange", "carlos@gmail.com");
+		Person p2 = new Person("Bob Brown", "bob@gmail.com");
+		Person p3 = new Person("Maria Green", "maria@gmail.com");
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		EntityManagerFactory emFactory = null;
+		// Instantiates an  EntityManager
+		EntityManager entityManager = null;
 		
+		
+		try {
+			// Instantiates an EntityManagerFactory
+			emFactory = Persistence.createEntityManagerFactory("exemplo-jpa");
+			// Instantiates an  EntityManager
+			entityManager = emFactory.createEntityManager();
+			
+			entityManager.getTransaction().begin();
+			
+			entityManager.persist(p1);
+			entityManager.persist(p2);
+			entityManager.persist(p3);
+			
+			entityManager.getTransaction().commit();
+			System.out.println("Done!");
+		} 
+		catch (Exception e) {
+			System.out.println("Error: "+ e.getMessage());
+		} 
+		finally {
+			if (entityManager != null) {
+				entityManager.close();				
+			}
+			if (emFactory != null) {
+				emFactory.close();
+			}
+		}
 	}
 
 }
